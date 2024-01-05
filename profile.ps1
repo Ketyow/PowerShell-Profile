7bea07db3cc08e3484162a2d5d33b366d8db00b0
Ketyow <Ketyow@users.noreply.github.com>
2024-01-05 09:04:10 +0100

    Update global settings

--- profile.ps1
+++ profile.ps1
@@ -1,6 +1,10 @@
-Write-Verbose -Message "Ustawienie zmiannych środowiskowych na potrzeby Proxy"
+Write-Verbose -Message "Set ciphers"
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType] 'Tls12, Tls13'
+
+Write-Verbose -Message "Set proxy settings"
 [System.Net.WebRequest]::DefaultWebProxy = [System.Net.WebRequest]::GetSystemWebProxy()
 [System.Net.WebRequest]::DefaultWebProxy.Credentials = [System.Net.CredentialCache]::DefaultNetworkCredentials
 
-Write-Verbose -Message "Logowanie transkrypcji do pliku"
-Start-Transcript -OutputDirectory $env:USERPROFILE\Documents\WindowsPowerShell\history -Append -NoClobber
+Write-Verbose -Message "Set shell loging history"
+Start-Transcript -OutputDirectory "$env:USERPROFILE\Documents\WindowsPowerShell\history" -Append -NoClobber
+
